74f6d39376c39db56326586bccb16af32cb9bde7
osmankozoglu <osmankozoglu@ogr.bandirma.edu.tr>
2024-06-03 04:30:52 +0300

    cors

--- backend/index.js
+++ backend/index.js
@@ -5,24 +5,18 @@ const cors = require('cors');
 
 const mailRouter = require('./routes/sendMail')
 
-
-
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json())
 
 app.use("/",(req,res)=>{
   res.send("Server is running.")
 })
 
 app.all("*", mailRouter);
-
-
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
